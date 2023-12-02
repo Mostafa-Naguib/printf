74312878351382836cca0755379db239dd158002
Elhlawany <aboabdallahhlawany@gmil.com>
2023-12-02 20:19:20 +0200

    upload main.h file

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* include a nessery libraries */
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <string.h>
+
+/* prototype functions */
+int _printf(const char *format, ...);
+
+
+
+#endif
