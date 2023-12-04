bc529bb9dfdf58803092f8b978b5d6d2bc9a989d
mostafa <mostafanaguib200@gmail.com>
2023-12-04 18:39:02 +0200

    testing

--- functions.c
+++ functions.c
@@ -1,25 +1,13 @@
 #include "main.h"
 
 /**
- * _putchar - Writes the character c to stdout
- *
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: 1 on success. -1 on error
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(int c)
 {
-	static int x;
-	static char buffer[BUFFER_SIZE];
-
-	if (c == BUFFER_FLUSH || x >= BUFFER_SIZE)
-	{
-		write(1, buffer, x);
-		x = 0;
-	}
-	if (c != BUFFER_FLUSH)
-		buffer[x++] = c;
-
-	return (x);
+	return (write(1, &c, 1));
 }
