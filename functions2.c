879e1f75106ac0dac1690dd0cfb0c33bf2386e4c
mostafa <mostafanaguib200@gmail.com>
2023-12-05 18:32:08 +0200

    test

--- functions2.c
+++ functions2.c
@@ -10,39 +10,28 @@
 
 int print_int(va_list args)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	int num = va_arg(args, int);
+	int divisor, digit;
+	int counter = 0;
 
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
+	if (num < 0)
 	{
 		_putchar('-');
 		num = -num;
-		n = -n;
-		last = -last;
-		i++;
+        counter++;
 	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
 
-	return (i);
+	divisor = 1;
+	while (num / divisor > 9)
+		divisor *= 10;
+
+	while (divisor != 0)
+	{
+		digit = num / divisor;
+		_putchar(digit + '0');
+		num %= divisor;
+		divisor /= 10;
+		counter++;
+	}
+	return (counter);
 }
