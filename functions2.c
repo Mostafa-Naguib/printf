ed55977c421d10b2ce14b4880fc74406101283a4
mostafa <mostafanaguib200@gmail.com>
2023-12-06 00:48:18 +0200

    last task

--- functions2.c
+++ functions2.c
@@ -11,10 +11,11 @@
 int print_int(va_list args)
 {
 	int num = va_arg(args, int);
-	int divisor, digit, last = num % 10;
+	int divisor, digit;
+	int last = num % 10;
 	int counter = 1;
 
-    num = num / 10;
+	num = num / 10;
 
 	if (last < 0)
 	{
@@ -24,23 +25,22 @@ int print_int(va_list args)
 		counter++;
 	}
 
-    if (num > 0)
-    {
-        divisor = 1;
-        while (num / divisor != 0)
-            divisor *= 10;
+	if (num > 0)
+	{
+		divisor = 10;
+		while (num / divisor > 9)
+			divisor *= 10;
 
-
-        while (divisor > 0)
-        {
-            digit = num / divisor;
-            _putchar(digit + '0');
-            num %= divisor;
-            divisor /= 10;
-            counter++;
-        }
-    }
-    _putchar(last + '0');
+		while (divisor != 0)
+		{
+			digit = num / divisor;
+			_putchar(digit + '0');
+			num %= divisor;
+			divisor /= 10;
+			counter++;
+		}
+	}
+	_putchar(last + '0');
 
 	return (counter);
 }
